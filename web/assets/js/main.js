2ed9d1ce9005a126fbe8b3c6d9d6c51f05b5923b
Zilin Xie <zxie43@uwo.ca>
2016-06-30 13:41:39 -0400

    map

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -453,36 +453,68 @@ $(document).ready(function() {
     });
 });
 
-$(document).ready(function () {
-	// create a LatLng object containing the coordinate for the center of the map
-	var latlng = new google.maps.LatLng(43.663133, -79.394754);
+function loadedGmap() {
+	if ($('body').hasClass("index-page")) {
+		// create a LatLng object containing the coordinate for the center of the map
+		var latlng = new google.maps.LatLng(-33.86455, 151.209);
 
-	// prepare the map properties
-	var options = {
-		zoom: 15,
-		center: latlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		navigationControl: true,
-		mapTypeControl: false,
-		scrollwheel: false,
-		disableDoubleClickZoom: true
-	};
+		// Find your styles here :) https://snazzymaps.com/explore
+		var styleArr = [{
+			"featureType": "landscape",
+			"stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]
+		}, {
+			"featureType": "poi",
+			"stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]
+		}, {
+			"featureType": "road.highway",
+			"stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+		}, {
+			"featureType": "road.arterial",
+			"stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]
+		}, {
+			"featureType": "road.local",
+			"stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]
+		}, {
+			"featureType": "transit",
+			"stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+		}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {
+			"featureType": "water",
+			"elementType": "labels",
+			"stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]
+		}, {
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]
+		}];
 
-	// initialize the map object
-	var map = new google.maps.Map(document.getElementById('google_map'), options);
+		// prepare the map properties
+		var options = {
+			zoom: 15,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			navigationControl: true,
+			mapTypeControl: false,
+			scrollwheel: false,
+			styles: styleArr,
+			disableDoubleClickZoom: true
+		};
 
-	// add Marker
-	var marker1 = new google.maps.Marker({
-		position: latlng, map: map
-	});
+		// initialize the map object
+		var map = new google.maps.Map(document.getElementById('google_map'), options);
 
-	// add listener for a click on the pin
-	google.maps.event.addListener(marker1, 'click', function () {
-		infowindow.open(map, marker1);
-	});
+		// add Marker
+		var marker1 = new google.maps.Marker({
+			position: latlng, map: map
+		});
 
-	// add information window
-	var infowindow = new google.maps.InfoWindow({
-		content: '<div class="info"><strong>Borderless</strong><br><br> 12 Hart House Circle<br> Toronto, ON</div>'
-	});
-});
+		// add listener for a click on the pin
+		google.maps.event.addListener(marker1, 'click', function () {
+			infowindow.open(map, marker1);
+		});
+
+		// add information window
+		var infowindow = new google.maps.InfoWindow({
+			content: '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
+		});
+	}
+};
